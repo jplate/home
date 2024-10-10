0403490be65a266ae6e15bdb8c349e9ef01fd634
jplate <jan.plate@gmail.com>
2024-10-10 12:22:04 +0900

    Update page.tsx

--- src/app/Research/page.tsx
+++ src/app/Research/page.tsx
@@ -9,17 +9,21 @@ export default function Page() {
                     My research generally focuses on the metaphysics of
                     intensional entities: properties, relations, states of
                     affairs, and other things of that ilk. In my most recent
-                    publication (&lsquo;
-                    <a
-                        href='https://philarchive.org/rec/PLAOTT'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                    >
-                        Ordinal Type Theory
-                    </a>
-                    &rsquo;), I develop an alternative to the &lsquo;Simple
-                    Theory of Types&rsquo; that is commonly used in the context
-                    of higher-order metaphysics. My current work focuses on the
+                    publication{' '}
+                    <span className='whitespace-nowrap'>
+                        (&lsquo;
+                        <a
+                            href='https://philarchive.org/rec/PLAOTT'
+                            target='_blank'
+                            rel='noopener noreferrer'
+                        >
+                            Ordinal Type Theory
+                        </a>
+                        &rsquo;),{' '}
+                    </span>
+                    I develop an alternative to the &lsquo;Simple Theory of
+                    Types&rsquo; that is commonly used in the context of
+                    higher-order metaphysics. My current work focuses on the
                     task of expanding this theory into a more full-fledged
                     ontology.
                 </p>
