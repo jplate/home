ad6fd9d4ff37210e4b6466a03d808004e61d89e2
jplate <jan.plate@gmail.com>
2024-10-11 13:50:07 +0900

    prettier

--- src/app/components/Content.tsx
+++ src/app/components/Content.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import dynamic from "next/dynamic";
+import dynamic from 'next/dynamic';
 
-const MyNavbar = dynamic(() => import('./Navbar'), {ssr: false,});
+const MyNavbar = dynamic(() => import('./Navbar'), { ssr: false });
 import MyFooter from './Footer';
 
 interface ContentDivProps {
