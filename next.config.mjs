55d11f273aec77f331af2e979aa2ff2071464a8e
jplate <jan.plate@gmail.com>
2024-10-11 14:27:26 +0900

    changing deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    //output: 'export',
+    output: 'export', // Enable static export for GitHub Pages
+    images: {
+      unoptimized: true, // Disable image optimization (necessary for static export)
+    },
+    basePath: '/home', // Set this to your GitHub repo name (without the username part)
+    assetPrefix: '/home/', // Same as basePath to properly serve assets
 };
 export default nextConfig;
