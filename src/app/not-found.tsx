63f586cfb536d11210848c5e19447ba58269a3f7
jplate <jan.plate@gmail.com>
2024-10-12 16:34:00 +0900

    prettier

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,21 +1,20 @@
 import Link from 'next/link';
-import Section from './components/Section'
+import Section from './components/Section';
 
 const NotFound = () => {
     return (
         <main className='relative min-h-screen'>
             <div className='absolute top-1/4 flex flex-col w-full items-center text-center'>
                 <Section id='main' header='404'>
+                    <p>The page you were looking for doesn&rsquo;t exist.</p>
                     <p>
-                        The page you were looking for doesn&rsquo;t exist.
-                    </p>
-                    <p>
-                        <Link href='/'>Here</Link> you can go back to the homepage.
+                        <Link href='/'>Here</Link> you can go back to the
+                        homepage.
                     </p>
                 </Section>
             </div>
         </main>
     );
-}
+};
 
 export default NotFound;
