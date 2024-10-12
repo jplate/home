14f1b01ef08afe5f48422babf795dc494e6c0966
jplate <jan.plate@gmail.com>
2024-10-12 16:32:08 +0900

    Create not-found.tsx

---
+++ src/app/not-found.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import Section from './components/Section'
+
+const NotFound = () => {
+    return (
+        <main className='relative min-h-screen'>
+            <div className='absolute top-1/4 flex flex-col w-full items-center text-center'>
+                <Section id='main' header='404'>
+                    <p>
+                        The page you were looking for doesn&rsquo;t exist.
+                    </p>
+                    <p>
+                        <Link href='/'>Here</Link> you can go back to the homepage.
+                    </p>
+                </Section>
+            </div>
+        </main>
+    );
+}
+
+export default NotFound;
