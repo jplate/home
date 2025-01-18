04489674e681de74c351e5aecc47890f1ef8e729
jplate <jan.plate@gmail.com>
2025-01-18 15:55:36 +0100

    more caching

--- next.config.mjs
+++ next.config.mjs
@@ -3,5 +3,19 @@ const nextConfig = {
     images: {
         minimumCacheTTL: 60,
     },
+    async headers() {
+        return [
+            {
+                source: '/(.*)', // Apply to all routes
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=3600', // Cache for 1 hour
+                    },
+                ],
+            },
+        ];
+    },
 };
+
 export default nextConfig;
