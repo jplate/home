e61f822144b192a9b3f02c66aeccf55293079672
jplate <jan.plate@gmail.com>
2025-08-15 21:02:20 +0200

    Fancier not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,29 @@ const NotFound = () => {
     return (
         <main>
             <div className='flex flex-col w-full items-center text-center mt-[25vh]'>
-                <Section id='main' header='404'>
+                {/* 404 Marquee */}
+                <div className='marquee-container mb-8'>
+                    <div className='marquee'>
+                        <span>404</span>
+                        <span>404</span>
+                        <span>404</span>
+                        <span>404</span>
+                        <span>404</span>
+                        <span>404</span>
+                        <span>404</span>
+                        <span>404</span>
+                        <span>404</span>
+                        <span>404</span>
+                        <span>404</span>
+                        <span>404</span>
+                        <span>404</span>
+                        <span>404</span>
+                        <span>404</span>
+                        <span>404</span>
+                    </div>
+                </div>
+                
+                <Section id='main' header='Page Not Found'>
                     <p>The page you were looking for doesn&rsquo;t exist.</p>
                     <p>
                         <Link href='/'>Here</Link> you can go back to the homepage.
