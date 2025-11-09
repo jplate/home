e1a2a9c2f804a062bc0ae550d17ff7628b0c6e6b
jplate <jan.plate@gmail.com>
2025-11-09 19:44:05 +0100

    Updated Research/page.tsx

--- src/app/Research/page.tsx
+++ src/app/Research/page.tsx
@@ -26,23 +26,18 @@ export default function Page() {
                 <p>
                     My research generally focuses on the metaphysics of intensional entities: properties,
                     relations, states of affairs, and other things of that ilk. In a recent{' '}
-                    <span className='whitespace-nowrap'>
-                        <a href='https://philarchive.org/rec/PLAOTT' target='_blank' rel='noopener'>
-                            paper
-                        </a>
-                    </span>{' '}
-                    I develop an alternative to the Simple Theory of Types that is commonly used in the
-                    context of higher-order metaphysics. I build on this in my forthcoming{' '}
-                    <span className='whitespace-nowrap'>
-                        &lsquo;
-                        <a href='https://philarchive.org/rec/PLATAO-12' target='_blank' rel='noopener'>
-                            Towards an Ontology of Roles and States
-                        </a>
-                        &rsquo;,{' '}
-                    </span>
-                    in which I propose a conception of relations as <em>sets of role sequences</em>. The
-                    tentative upshot is an ontology in which there are no &lsquo;complex&rsquo; entities other
-                    than states of affairs and roles.
+                    <a href='https://philarchive.org/rec/PLAOTT' target='_blank' rel='noopener'>
+                        paper
+                    </a>{' '}
+                    I develop an alternative (which I call &lsquo;Ordinal Type Theory&rsquo;) to the Simple
+                    Theory of Types that is commonly used in the context of higher-order metaphysics. I build
+                    on this in my forthcoming &lsquo;
+                    <a href='https://philarchive.org/rec/PLATAO-12' target='_blank' rel='noopener'>
+                        Towards an Ontology of Roles and States
+                    </a>
+                    &rsquo;, in which I propose a conception of relations as <em>sets of role sequences</em>.
+                    The tentative upshot is an ontology in which there are no &lsquo;complex&rsquo; entities
+                    other than states of affairs and roles.
                 </p>
             </Section>
         </Content>
